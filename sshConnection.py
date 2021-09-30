1b7cce933046a78b3a6b3d47128a557c2edaab9c
Tautvydas Diksas <tautvydas.diksas@gmail.com>
2021-09-30 13:50:20 +0300

    new

--- sshConnection.py
+++ sshConnection.py
@@ -5,6 +5,4 @@ class Connection:
   def returnConnection(self,connectionInfo):
     pass
 
-ser = serial.Serial('COM3', 38400, timeout=0,parity=serial.PARITY_EVEN, rtscts=1)
-s= ser.read(100)
-print(s)
+
